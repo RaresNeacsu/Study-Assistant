3f6e8f53baad2b89fe3c54def22dbe749e080114
RaresNeacsu <neacsurares2006@gmail.com>
2025-10-21 18:28:37 +0300

    Added direct file path to task.json and changed main(new command line for adding and deleting)

--- app/data_manager.py
+++ app/data_manager.py
@@ -1,11 +1,16 @@
 import json
-def load_json(file='task.json'):
+import os
+
+BASEDIR = os.path.dirname(os.path.dirname(__file__))
+BASETASKFILE = os.path.join(BASEDIR,".gitignore", 'task.json')
+
+def load_json(file=BASETASKFILE):
     try:
         with open(file) as f:
             return [json.loads(line) for line in f]
     except FileNotFoundError:
         return None
-def save_json(data, file='task.json'):
+def save_json(data, file=BASETASKFILE):
     with open(file, 'a') as f:
         for entry in data:
             f.write(json.dumps(entry) + "\n")
--- main.py
+++ main.py
@@ -1,7 +1,27 @@
+import sys
+import json
 from app.data_manager import load_json, save_json
 from app.time_utilities import zile_deadline
 
-save_json([{"Title": "Examen Algebra", "Deadline": "2025-11-17"}])
-for task in load_json():
-    zile_ramase = zile_deadline(task["Deadline"])
-    print({task["Title"]}, "-", zile_deadline(task["Deadline"]),"zile ramase")
+cmd=input("Command(add, delete, list): ")
+
+if(cmd=="add"):
+    title=input("Title: ")
+    deadline=input("Deadline(YYYY-MM-DD): ")
+    save_json([{"Title":title, "Deadline":deadline}])
+
+if(cmd=="delete"):
+    title=input("Title to delete: ")
+    tasks=load_json()
+    for task in tasks:
+        if task["Title"]==title:
+            tasks.remove(task)
+        with open(tasks, 'w') as f:
+            for entry in tasks:
+                f.write(json.dumps(entry) + "\n")
+
+if(cmd=="list"):
+    tasks=load_json()
+    for task in tasks:
+        zile_ramase = zile_deadline(task["Deadline"])
+        print({task["Title"]}, "-", zile_deadline(task["Deadline"]),"zile ramase")
